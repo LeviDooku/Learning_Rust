f2c51591e9ac49fc5b383cf7ca9d83de76a0108e
LeviDooku <pedrovelascosantana@gmail.com>
2024-05-23 09:06:13 +0200

    hola mundo en rust

---
+++ nivel_0/main.rs
@@ -0,0 +1,14 @@
+///////////////////////////
+//Pedro Velasco Santana  //
+//                       //
+//Archivo: main.rs       //
+///////////////////////////
+
+//Primer contacto con Rust
+
+//Para compilar: rustc archivo.rs
+
+fn main(){
+    let mensaje = "Hola mundo!"; //Crear una variable que muestra un mensaje
+    println!("{}", mensaje); //Imprimir el mensaje
+}
